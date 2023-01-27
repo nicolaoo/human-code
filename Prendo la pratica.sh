8744048126285e736f10c224f5599e164700bfab
nicola00 <nicolajobs00@gmail.com>
2023-01-27 16:48:53 +0100

    FEAT esercizio sulla scann del file gruppo6

---
+++ Prendo la pratica.sh
@@ -0,0 +1,46 @@
+Prendo la pratica
+-mi alzo dalla sedia
+    SE la porta è aperta
+esco dal mio ufficio
+    ALTRIMENTI
+la apro.
+
+
+-Mi dirigo verso la stampante
+    SE è occupata
+aspetto il mio turno
+    ALTRIMENTI 
+procedo.
+
+
+-Imposto modalità scan
+PER OGNI foglio alzo il coperchio
+posiziono il foglio dal lato a
+abbasso il coperchio
+premo il pulsante scan
+    
+    SE la scansione è andata a buon fine
+salvo il file
+alzo il coperchio
+prendo il foglio
+lo posiziono dal lato b
+abbasso il coperchio
+premo il pulsante scan
+
+    SE la scansione è andata a buon fine
+salvo il file
+alzo il coperchio
+prendo un foglio
+lo poggio nel gruppo dei fogli scansionati
+
+ALTRIMENTI
+    premo il pulsante scan
+
+ALTRIMENTI premo di nuovo il pulsante scan.
+
+-Prendo la pratica
+ritorno nel mio ufficio
+mi siedo alla scrivania
+creo la cartella della pratica sul desktop
+apro la cartella condivisa degli scan
+copio ed incollo i file nella mia cartella.
